2f31f09c4a907e009e1f7db2174cc2c18bf0e606
河原日出夫 <kawahide.72@gmail.com>
2021-06-07 23:45:39 +0900

    add: コールバック作成

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,13 +1,45 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
+  void checkBoxCallback(bool checkBoxState) {
+    setState(() {
+      isChecked = checkBoxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('task'),
-      trailing: Checkbox(
-        value: true,
+      title: Text(
+        'task',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
       ),
+      trailing: TaskCheckBox(
+          checkBoxState: isChecked, toggleCheckBoxState: checkBoxCallback),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool checkBoxState;
+  final Function toggleCheckBoxState;
+
+  TaskCheckBox({this.checkBoxState, this.toggleCheckBoxState});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.teal,
+      value: checkBoxState,
+      onChanged: toggleCheckBoxState,
     );
   }
 }
