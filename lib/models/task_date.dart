f7a1e02b55820ecfe32db62856f07400b065255e
河原日出夫 <kawahide.72@gmail.com>
2021-06-12 16:12:35 +0900

    fix: プライベートにリファクタ

--- lib/models/task_date.dart
+++ lib/models/task_date.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'task.dart';
+import 'dart:collection';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'buy toy'),
     Task(name: 'buuy buuy'),
     Task(name: 'buuuuuu'),
   ];
 
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
   void addTask(String newTaskTitle) {
     final task = Task(name: newTaskTitle);
-    tasks.add(task);
+    _tasks.add(task);
     notifyListeners();
   }
 }
