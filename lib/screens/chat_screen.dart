eb6468b37d36cdff70b3a2c5563147a211a0a7e9
河原日出夫 <kawahide.72@gmail.com>
2021-09-25 19:50:19 +0900

    feat: メッセージ一覧作成

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -17,9 +17,126 @@ class ChatScreen extends StatelessWidget {
   }
 }
 
+class User {
+  User({
+    @required this.name,
+    @required this.message,
+    @required this.path,
+  });
+
+  final String name;
+  final String path;
+  final String message;
+}
+
 class ChatScreenBody extends StatelessWidget {
+  List<Color> colorList = [Colors.cyan, Colors.deepOrange, Colors.indigo];
+
+  final List<User> userList = [
+    User(
+      name: 'やまだ',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person1.jpeg',
+    ),
+    User(
+      name: 'たなか',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person2.jpeg',
+    ),
+    User(
+      name: 'すずき',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person3.jpeg',
+    ),
+    User(
+      name: 'やまだ',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person1.jpeg',
+    ),
+    User(
+      name: 'たなか',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person2.jpeg',
+    ),
+    User(
+      name: 'すずき',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person3.jpeg',
+    ),
+    User(
+      name: 'やまだ',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person1.jpeg',
+    ),
+    User(
+      name: 'たなか',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person2.jpeg',
+    ),
+    User(
+      name: 'すずき',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person3.jpeg',
+    ),
+    User(
+      name: 'やまだ',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person1.jpeg',
+    ),
+    User(
+      name: 'たなか',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person2.jpeg',
+    ),
+    User(
+      name: 'すずき',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person3.jpeg',
+    ),
+    User(
+      name: 'やまだ',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person1.jpeg',
+    ),
+    User(
+      name: 'たなか',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person2.jpeg',
+    ),
+    User(
+      name: 'すずき',
+      message: 'こんにちは！よろしくお願い！',
+      path: 'images/dummy/dummy_person3.jpeg',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 80,
+            color: Colors.teal[200],
+            // 何かのバナーを入れる
+          ),
+          ListView.builder(
+            itemCount: userList.length,
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true,
+            itemBuilder: (BuildContext context, int index) {
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: AssetImage(userList[index].path),
+                ),
+                title: Text(userList[index].name),
+                subtitle: Text(userList[index].message),
+                trailing: Text('1日前'),
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
