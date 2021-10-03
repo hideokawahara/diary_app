2b93cc21692268c72285c13ebf5c3dd7c1cfb616
河原日出夫 <kawahide.72@gmail.com>
2021-10-03 22:50:28 +0900

    feat: 自己紹介ステータス作成

--- lib/screens/my_profile_screen.dart
+++ lib/screens/my_profile_screen.dart
@@ -26,10 +26,64 @@ class MyProfileScreenBody extends StatelessWidget {
       width: double.infinity,
       child: Column(
         children: <Widget>[
-          CircleAvatar(
-            radius: 110.0,
-            backgroundImage: AssetImage(
-              'images/dummy/myPage_icon.jpg',
+          Container(
+            // width: double.infinity,
+            decoration: BoxDecoration(
+                // color: Colors.blueAccent,
+                // image: DecorationImage(
+                //     image: AssetImage(
+                //       'images/dummy/dummy_person1.jpeg',
+                //     ),
+                //     fit: BoxFit.cover),
+                ),
+            child: CircleAvatar(
+              radius: 110.0,
+              backgroundImage: AssetImage(
+                'images/dummy/myPage_icon.jpg',
+              ),
+            ),
+          ),
+          Container(
+            color: Colors.black12,
+            padding: const EdgeInsets.fromLTRB(
+              16.0,
+              24.0,
+              16.0,
+              0,
+            ),
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'でおでお 25歳 大阪',
+                ),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.bubble_chart_outlined,
+                    ),
+                    Text(
+                      'よろしく！',
+                    ),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Container(
+                      width: 10.0,
+                      height: 10.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(25.0),
+                        color: Colors.green,
+                      ),
+                    ),
+                    Text(
+                      'オンライン',
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ],
